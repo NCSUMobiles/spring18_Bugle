010f2445061068d12cbaa5d127bdc54a61230cf7
zhulinn <julian.lin.zhu@gmail.com>
2018-04-07 03:53:32 +0000

    approve & reject action

--- Bugle/index.js
+++ Bugle/index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var server = express();
 server.use(express.static(__dirname + '/www'));
- 
+ server.use(express.static(__dirname + '/www/views'));
 var port = 3000;
 server.listen(port, function() {
     console.log('server listening on port ' + port + ' visit http://localhost:3000 ');
