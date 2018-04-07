637c4fc2416ad8b0fdbf12060272ed1c2b9ef742
Sumit Srivastava <ssrivas8@ncsu.edu>
2018-04-07 03:52:52 -0400

    Update index.js
    port for Heroku.

--- Bugle/index.js
+++ Bugle/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var server = express();
 server.use(express.static(__dirname + '/www'));
  server.use(express.static(__dirname + '/www/views'));
-var port = 3000;
+var port = process.env.PORT || 5000;
 server.listen(port, function() {
     console.log('server listening on port ' + port + ' visit http://localhost:3000 ');
 });
